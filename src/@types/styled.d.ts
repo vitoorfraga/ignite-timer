a07cadfa55ed040b4c27ddadc6c5f5ef5dd1f009
Vitor Fraga <vitorfragaps@gmail.com>
2023-06-09 23:04:59 -0300

    feat: componente de Home estilizado.

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -4,7 +4,7 @@ import { defaultTheme } from '../styles/themes/default'
 // => Acessar valores do tema e guardar dentro de uma variavel.
 type ThemeType = typeof defaultTheme
 
-// => Criando uma tipagem para o modulo styled-components
+// => Criando uma tipagem para o modulo styled-components do npm
 declare module 'styled-components' {
   export interface DefaultTheme extends ThemeType {}
 }
