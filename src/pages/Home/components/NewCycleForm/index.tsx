7d9fd78688c88738ca57d74256956f89983280e3
Vitor Fraga <vitorfragaps@gmail.com>
2023-08-18 21:37:06 -0300

    Reset do formulário.

--- src/pages/Home/components/NewCycleForm/index.tsx
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -3,7 +3,7 @@ import { FormContainer, MinutesAmountInput, TaskInput } from './styles'
 import { useContext } from 'react'
 
 import { useFormContext } from 'react-hook-form'
-import { CyclesContext } from '../../../contexts/CyclesContext'
+import { CyclesContext } from '../../../../contexts/CyclesContext'
 
 export function NewCycleForm() {
   const { activeCycle } = useContext(CyclesContext)
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,63 +1,66 @@
 import { HandPalm, Play } from 'phosphor-react'
+import { FormProvider, useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+import { useContext } from 'react'
 
 import {
   HomeContainer,
   StartCountdownButton,
   StopCountdownButton,
 } from './styles'
-import { useContext } from 'react'
-import { Countdown } from './Countdown'
-import { NewCycleForm } from './NewCycleForm'
-import { FormProvider, useForm } from 'react-hook-form'
-import { zodResolver } from '@hookform/resolvers/zod'
-import * as zod from 'zod'
+
 import { CyclesContext } from '../../contexts/CyclesContext'
+import { NewCycleForm } from './components/NewCycleForm'
+import { Countdown } from './components/Countdown'
+
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod
+    .number()
+    .min(5, 'O ciclo precisa ser de no mínimo 5 minutos.')
+    .max(60, 'O ciclo precisa ser de no máximo 60 minutos.'),
+})
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
 export function Home() {
   const { activeCycle, createNewCycle, interruptCurrentCycle } =
     useContext(CyclesContext)
 
-  const newCycleFormValidationScheme = zod.object({
-    task: zod.string().min(1, 'Informe a tarefa'),
-    minutesAmount: zod
-      .number()
-      .min(1, 'O ciclo deve ser de no mínimo 5 minutos.')
-      .max(60, 'O clico deve ser de no máximo 60 minutos.'),
-  })
-
-  type NewCycleFormData = zod.infer<typeof newCycleFormValidationScheme>
-
   const newCycleForm = useForm<NewCycleFormData>({
-    resolver: zodResolver(newCycleFormValidationScheme),
+    resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
       task: '',
       minutesAmount: 0,
     },
   })
 
-  const { handleSubmit, watch /* reset */ } = newCycleForm
+  const { handleSubmit, watch, reset } = newCycleForm
+
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    createNewCycle(data)
+    reset()
+  }
 
   const task = watch('task')
-  const isSubmitDisabled = !task
+  const isSubmitDisable = !task
 
   return (
     <HomeContainer>
-      <form action="" onSubmit={handleSubmit(createNewCycle)}>
+      <form onSubmit={handleSubmit(handleCreateNewCycle)}>
         <FormProvider {...newCycleForm}>
           <NewCycleForm />
         </FormProvider>
         <Countdown />
+
         {activeCycle ? (
-          <StopCountdownButton
-            type="button"
-            onClick={interruptCurrentCycle}
-            disabled={!activeCycle}
-          >
+          <StopCountdownButton onClick={interruptCurrentCycle} type="button">
             <HandPalm size={24} />
             Interromper
           </StopCountdownButton>
         ) : (
-          <StartCountdownButton type="submit" disabled={isSubmitDisabled}>
+          <StartCountdownButton disabled={isSubmitDisable} type="submit">
             <Play size={24} />
             Começar
           </StartCountdownButton>
